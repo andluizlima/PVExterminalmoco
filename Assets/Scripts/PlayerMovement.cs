1f095338033ee7ee230b4a33e8e3d18f3b9ea505
andluizlima <rodrigues_andre@id.uff.br>
2017-05-27 23:37:01 -0300

    Primeiro commit

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+	public float speed;
+	public float rotationSpeed;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position += Time.deltaTime * speed * transform.forward * Input.GetAxis("Vertical");
+		transform.RotateAround (transform.position, transform.up, Input.GetAxis("Horizontal") * Time.deltaTime * rotationSpeed);
+	}
+}
